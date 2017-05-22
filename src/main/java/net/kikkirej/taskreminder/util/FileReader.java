6bf64f21a346585eef1e421f5b5b3b0cdb47018c
Fabian Lemke <info@kikkirej.net>
2017-05-22 19:36:43 +0200

    created a File util

---
+++ src/main/java/net/kikkirej/taskreminder/util/FileReader.java
@@ -0,0 +1,45 @@
+package net.kikkirej.taskreminder.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+public class FileReader {
+	
+	private File file;
+
+	public FileReader(String path) {
+		file = new File(path);
+	}
+	
+	public FileReader(File file) {
+		this.file = file;
+	}
+	
+	/**
+	 * 
+	 * @return all lines in {@link List}
+	 * @throws IOException is thrown if there is a error while reading
+	 */
+	public List<String> getLineArray() throws IOException {
+		List<String> lines = FileUtils.readLines(file, Charset.defaultCharset());
+		return lines;
+	}
+	
+	/**
+	 * @return all lines of text as one {@link String}
+	 * @throws IOException is thrown if there is a error while reading
+	 */
+	public String getAllText() throws IOException{
+		List<String> lines = getLineArray();
+		String returnValue = "";
+		for (String string : lines) {
+			returnValue += string + "\n";
+		}
+		return returnValue;
+	}
+	
+}
