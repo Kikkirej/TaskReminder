3424b4fab1d899959a1dc6c3ff558342fe713863
Fabian Lemke <info@kikkirej.net>
2017-05-22 20:23:27 +0200

    advanced Tst

--- src/test/java/net/kikkirej/taskreminder/preferences/MappingGeneratorTest.java
+++ src/test/java/net/kikkirej/taskreminder/preferences/MappingGeneratorTest.java
@@ -31,6 +31,31 @@ public class MappingGeneratorTest {
 		assertEquals(expected, mappings.get(0));
 	}
 	
+	@Test
+	public void multiMapping() throws Exception {
+		ArrayList<String> mappingText = new ArrayList<String>();
+		mappingText.add("Hans Mustermann, pc123, hans.mustermann@mail.de, true");
+		mappingText.add("Somebody, hostname, mail@mail, false");
+		mappingGenerator = new MappingGenerator(mappingText);
+		List<MappingObject> mappings = mappingGenerator.getMappings();
+		if(mappings == null || mappings.size()!= 2){
+			fail("Die Rückgabe ist fehlerhaft.");
+		}
+		MappingObject expected1 = new MappingObject();
+		expected1.hostname = "pc123";
+		expected1.name = "Hans Mustermann";
+		expected1.shouldGetMail = true;
+		expected1.mail = "hans.mustermann@mail.de";
+		assertEquals(expected1, mappings.get(0));
+		
+		MappingObject expected2 = new MappingObject();
+		expected2.hostname = "hostname";
+		expected2.name = "Somebody";
+		expected2.shouldGetMail = false;
+		expected2.mail = "mail@mail";
+		assertEquals(expected2, mappings.get(1));
+	}
+	
 	public void tearDown() {
 		mappingGenerator = null;
 	}
