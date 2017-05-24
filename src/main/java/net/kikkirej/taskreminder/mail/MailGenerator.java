8e608a64470f4b915b7d561a79593d5180a7db7a
Carina <Carina@Lars>
2017-05-24 11:54:58 +0200

    mailgenerator

--- src/main/java/net/kikkirej/taskreminder/mail/MailGenerator.java
+++ src/main/java/net/kikkirej/taskreminder/mail/MailGenerator.java
@@ -1,14 +1,25 @@
 package net.kikkirej.taskreminder.mail;
 
+import java.io.IOException;
+
 import net.kikkirej.taskreminder.model.MailObject;
 import net.kikkirej.taskreminder.model.MappingObject;
 import net.kikkirej.taskreminder.model.TaskObject;
+import net.kikkirej.taskreminder.util.FileReader;
 
 public class MailGenerator {
 
+	MailObject mail = new MailObject();
+	FileReader file;
+	String pathPlaceholder;
+	
 	public MailObject getNewUserMail(TaskObject taskObject) {
-		// TODO Auto-generated method stub
-		return null;
+		String path = "";
+		String admin = "";
+		mail.Message = erzeugeMessage(path);
+		//Empfänger ist der, der in der Konfig als Admin eingetragen ist
+		mail.recipents.add(admin);
+		return mail;
 	}
 
 	public MailObject getSimpleReminder(MappingObject mapping, String taskname) {
@@ -18,9 +29,33 @@ public class MailGenerator {
 	}
 
 	public MailObject getAlertmail(TaskObject taskObject) {
-		// TODO Auto-generated method stub
+		// noch hostname
 		return null;
 		
+	}
+	
+	private String erzeugeMessage(String path) {
+		String message;
+		try {
+			message = new FileReader("path").getAllText();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			message = "";
+		}
+		return ersetzePlatzhalter(message);
+	}
+
+	public String ersetzePlatzhalter(String message) {
+		String platzhalter;
+		try {
+			platzhalter = new FileReader(pathPlaceholder).getAllText();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		platzhalter = "";
+		return message;
 	}
 
 }
---
+++ src/test/java/net/kikkirej/taskreminder/mail/generator/MailGeneratorTest.java
@@ -0,0 +1,54 @@
+package net.kikkirej.taskreminder.mail.generator;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.mail.Email;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import net.kikkirej.taskreminder.mail.MailGenerator;
+import net.kikkirej.taskreminder.model.MailObject;
+import net.kikkirej.taskreminder.model.TaskObject;
+
+public class MailGeneratorTest {
+
+	MailGenerator mailGenerator;
+	
+	@Before
+	public void setUp() throws Exception {
+		mailGenerator = new MailGenerator();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void getNewUserMailTest() throws Exception{
+		//Arrange
+		String message="blablabla";
+		TaskObject task = new TaskObject();
+		//Act
+		MailObject expected = mailGenerator.getNewUserMail(task);
+		//Assert
+		assertEquals(expected.Message, message);
+	}
+	
+	
+	@Test
+	public void ersetzePlatzhalterTest() throws Exception{
+		//Arrange
+		String message="Es ist ein unbekannter User vorhanden \n Kudo";
+		
+		String platzhalter="Username";
+		String eingabe="Es ist ein unbekannter User vorhanden \n PERSONENNAME";
+		//Act
+		String expected = mailGenerator.ersetzePlatzhalter(eingabe, platzhalter);
+		//Assert
+		assertEquals(expected, message);
+	}
+
+}
