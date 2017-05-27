6e6ed2e2175f780c4143ec7bd2a1b91e56cad705
Fabian Lemke <info@kikkirej.net>
2017-05-27 21:26:12 +0200

    TaskChecker implemented

--- modules/core/src/main/java/net/kikkirej/taskreminder/TaskChecker.java
+++ modules/core/src/main/java/net/kikkirej/taskreminder/TaskChecker.java
@@ -1,11 +1,23 @@
 package net.kikkirej.taskreminder;
 
 import net.kikkirej.taskreminder.model.TaskObject;
+import net.kikkirej.taskreminder.preferences.PreferenceKeys;
+import net.kikkirej.taskreminder.preferences.PreferenceManagerSingleton;
 
 public class TaskChecker {
 
+	private String[] csvValues;
+
+	public TaskChecker() {
+		csvValues = PreferenceManagerSingleton.getInstance().getCSVValues(PreferenceKeys.IMPORTANTTASKS, ';');
+	}
+	
 	public boolean isTaskRelevant(TaskObject taskObject) {
-		// TODO Auto-generated method stub
+		for (String csvValue : csvValues) {
+			if(csvValue.equals(taskObject.taskname)){
+				return true;
+			}
+		}
 		return false;
 	}
 
