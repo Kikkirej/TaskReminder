d6df64c5579af251ceb386e7592570f46dc2062d
Fabian Lemke <info@kikkirej.net>
2017-05-27 21:56:56 +0200

    Test zurückgestellt und Imports organisiert

--- modules/core/src/test/java/net/kikkirej/taskreminder/network/HostcheckerTest.java
+++ modules/core/src/test/java/net/kikkirej/taskreminder/network/HostcheckerTest.java
@@ -1,9 +1,8 @@
 package net.kikkirej.taskreminder.network;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class HostcheckerTest {
